1b733cef59ff69e38f220d25c990bbb95d560c38
connell <gonnellcough@gmail.com>
2017-10-21 13:34:18 -0700

    alrighty now

--- qapp/views.py
+++ qapp/views.py
@@ -335,16 +335,16 @@ class PhotoViewSet(APIView):  #need to issue tokens for anon users and logged in
             uuid = photo.uuid
            
             comments = Photo.return_comments(uuid)
-            data={'uuid':'photo.uuid','lat':photo.lat,'lon':photo.lon,'poster':photo.poster,'timestamp':'photo.timestamp',\
-'caption':photo.caption,'useruuid':'photo.useruuid','photo_distance':photo.distance,\
-'comments':[{'photouuid':'com.photouuid','comments':com.comment,'poster':com.poster,'timestamp':'com.timestamp','uuid':'com.uuid','useruuid':'com.useruuid'} for com in comments]}
+            data={'uuid':photo.uuid,'lat':photo.lat,'lon':photo.lon,'poster':photo.poster,'timestamp':photo.timestamp,\
+'caption':photo.caption,'useruuid':photo.useruuid,'photo_distance':photo.distance,\
+'comments':[{'photouuid':com.photouuid,'comments':com.comment,'poster':com.poster,'timestamp':com.timestamp,'uuid':com.uuid,'useruuid':com.useruuid} for com in comments]}
             photos['photo' + str(counter)] = data
         
             counter +=1
         #photos = json.dumps(photos,indent=4, separators=(',', ': '))
         #JSONRenderer().render(photos)
         print(type(photos))
-        return Response(photos, status=status.HTTP_200_OK, content_type='json')
+        return Response(photos, status=status.HTTP_200_OK, headers={'Content-Type': 'application/json'})
 
 
 
