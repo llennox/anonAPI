2639e42f2919b04d58dac58e40494b99e35cd8fc
connell <gonnellcough@gmail.com>
2017-10-21 20:04:14 -0700

    heeellllllytyeaara

--- qapp/views.py
+++ qapp/views.py
@@ -325,7 +325,7 @@ class PhotoViewSet(APIView):  #need to issue tokens for anon users and logged in
         lat1 = self.roundGET(args[0], 5)
         lon1 = self.roundGET(args[1], 5)
         #serializer = PhotoSerializer(photos, many=True)
-        photos = {}
+        photos = []
         counter = 1
         for photo in photos1: # do the haversin and attach comments proly a new litt func 
             data = {}
@@ -335,10 +335,12 @@ class PhotoViewSet(APIView):  #need to issue tokens for anon users and logged in
             uuid = photo.uuid
            
             comments = Photo.return_comments(uuid)
-            data=[{'uuid':photo.uuid,'lat':photo.lat,'lon':photo.lon,'poster':photo.poster,'timestamp':photo.timestamp,\
+            data={'uuid':photo.uuid,'lat':photo.lat,'lon':photo.lon,'poster':photo.poster,'timestamp':photo.timestamp,\
 'caption':photo.caption,'useruuid':photo.useruuid,'photo_distance':photo.distance,\
-'comments':[{'photouuid':com.photouuid,'comments':com.comment,'poster':com.poster,'timestamp':com.timestamp,'uuid':com.uuid,'useruuid':com.useruuid} for com in comments]}]
-            photos['photo' + str(counter)] = data
+'comments':[{'photouuid':com.photouuid,'comments':com.comment,'poster':com.poster,'timestamp':com.timestamp,'uuid':com.uuid,'useruuid':com.useruuid} for com in comments]}
+
+
+            photos.append(data) #I need to pudate here instead of doing this stupid counter
         
             counter +=1
         #photos = json.dumps(photos,indent=4, separators=(',', ': '))
